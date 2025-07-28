4bdf7190f355b62c1ed88a3a8f9a6db2cc13cbd0
Farid-Efrei <farid.djerrar@efrei.net>
2025-07-28 22:07:02 +0200

    feat: integrate theme provider and enable dark mode toggle in tabs layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,9 @@
+import useTheme from "@/hooks/useTheme";
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const { toggleDarkMode } = useTheme();
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
@@ -9,6 +11,9 @@ export default function Index() {
       </Text>
       <Text>Helloooo</Text>
       <Link href="/about">Visit la page about</Link>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle Dark Mode</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -18,7 +23,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "palegreen",
+    // backgroundColor: "palegreen",
   },
   content: {
     fontSize: 20,
