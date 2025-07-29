da6d2548ea74001ba0f84dde0f2c0a3eab768552
Fairytale-DEF <farid.djerrar@efrei.net>
2025-07-29 17:06:41 +0200

    feat: integrate ConvexProvider and add todos query and mutation in index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,18 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+
+  const todos = useQuery(api.todos.getTodos)
+  console.log("Todos:", todos);
+
+  const addTodo = useMutation(api.todos.addTodo)
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.content}>
@@ -13,6 +22,10 @@ export default function Index() {
       <Link href="/about">Visit la page about</Link>
       <TouchableOpacity onPress={toggleDarkMode}>
         <Text>Toggle Dark Mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=> addTodo({ text: "Titiiii !" })}>;
+      
+        <Text>Add a new TODO</Text>
       </TouchableOpacity>
     </View>
   );
