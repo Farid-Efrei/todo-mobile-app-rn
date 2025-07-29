d88608e1c4d80d45874cfbafc10a528e29ccd3c2
Fairytale-DEF <farid.djerrar@efrei.net>
2025-07-29 19:00:54 +0200

    feat: add home and settings styles for improved UI layout and theming

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
+import { createHomeStyles } from "@/assets/styles/home.styles";
 import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
 import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
+
+  // const styles = createStyles(colors);
+  const homeStyles = createHomeStyles(colors);
 
   const todos = useQuery(api.todos.getTodos)
   console.log("Todos:", todos);
@@ -15,8 +21,8 @@ export default function Index() {
   
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>
+    <SafeAreaView style={homeStyles.safeArea}>
+      <Text style={homeStyles.loadingText}>
         Edit app/index.tsx to edit this screen123.
       </Text>
       <Text>Helloooo</Text>
@@ -32,19 +38,36 @@ export default function Index() {
       
         <Text>Clear all the todos</Text>
       </TouchableOpacity> */}
-    </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "palegreen",
-  },
-  content: {
-    fontSize: 20,
-    color: "darkred",
-  },
-});
+
+// const createStyles = (colors : ColorScheme ) => {
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       justifyContent: "center",
+//       alignItems: "center",
+//       backgroundColor: colors.bg,
+//     },
+//     content: {
+//       fontSize: 20,
+//       color: colors.text,
+//     },
+//   });
+//   return styles;
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "palegreen",
+//   },
+//   content: {
+//     fontSize: 20,
+//     color: "darkred",
+//   },
+// });
