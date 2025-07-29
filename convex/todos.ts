efd3b698255f27d642e617c5d7bb4dad0dd2ff76
Fairytale-DEF <farid.djerrar@efrei.net>
2025-07-29 16:30:01 +0200

    feat: add addTodo mutation to insert new todos into the database

--- convex/todos.ts
+++ convex/todos.ts
@@ -1,8 +1,34 @@
-import { query } from './_generated/server'
+import { v } from 'convex/values'
+import { mutation, query } from './_generated/server'
 
 export const getTodos = query({
   handler: async (ctx) => {
    const todos = await ctx.db.query("todos").order("desc").collect()
     return todos
+  }
+})
+
+
+export const addTodo = mutation({
+  args: { text: v.string() },
+  handler: async(ctx, args) => {
+    const todoId = await ctx.db.insert("todos", {
+      text: args.text,
+      isCompleted: false
+    })
+    return todoId
+  }
+}) 
+
+export const toggleTodo = mutation({
+  args: { todoId: v.id("todos") },
+  handler: async (ctx, args) => {
+    const todo = await ctx.db.get(args.todoId)
+    if (!todo) {
+      throw new Error("Todo not found")
+    }
+    await ctx.db.patch(args.todoId, {
+      isCompleted: !todo.isCompleted
+    })
   }
 })
