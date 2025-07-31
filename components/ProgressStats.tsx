dac5c6a22418d423a476343d1aa119de284b9624
Fairytale-DEF <farid.djerrar@efrei.net>
2025-07-31 16:20:20 +0200

    feat: display completed and active todos in ProgressStats component

--- components/ProgressStats.tsx
+++ components/ProgressStats.tsx
@@ -25,17 +25,19 @@ const ProgressStats = () => {
         Stats de Progression
       </Text>
 
+<View style={settingsStyle.statsContainer}>
+
 
       {/* TOTAL TODOS */}
       <LinearGradient
         colors={colors.gradients.background}
         style={[settingsStyle.statCard, {borderLeftColor: colors.primary}]}
-      >
+        >
         <View style={settingsStyle.statIconContainer}>
       <LinearGradient
         colors={colors.gradients.primary}
         style={settingsStyle.statIcon}
-      >
+        >
         <Ionicons name='list' size={24} color={colors.text} />
       </LinearGradient>
         </View>
@@ -47,6 +49,53 @@ const ProgressStats = () => {
         </View>
 
       </LinearGradient>
+
+
+{/* COMPLETED TODOS */}
+       <LinearGradient
+        colors={colors.gradients.background}
+        style={[settingsStyle.statCard, {borderLeftColor: colors.success}]}
+        >
+        <View style={settingsStyle.statIconContainer}>
+      <LinearGradient
+        colors={colors.gradients.success}
+        style={settingsStyle.statIcon}
+        >
+        <Ionicons name='checkmark-circle' size={24} color={colors.text} />
+      </LinearGradient>
+        </View>
+
+        <View >
+          <Text style={settingsStyle.statNumber}>{completedTodos}</Text>
+          <Text style={settingsStyle.statLabel}>Tâches Complétées</Text>
+
+        </View>
+
+      </LinearGradient>
+
+
+      {/* ACTIVE TODOS */}
+       <LinearGradient
+        colors={colors.gradients.background}
+        style={[settingsStyle.statCard, {borderLeftColor: colors.warning}]}
+        >
+        <View style={settingsStyle.statIconContainer}>
+      <LinearGradient
+        colors={colors.gradients.warning}
+        style={settingsStyle.statIcon}
+        >
+        <Ionicons name='time' size={24} color={colors.text} />
+      </LinearGradient>
+        </View>
+
+        <View >
+          <Text style={settingsStyle.statNumber}>{activeTodos}</Text>
+          <Text style={settingsStyle.statLabel}>Tâches Actives</Text>
+
+        </View>
+
+      </LinearGradient>
+        </View>
     </LinearGradient>
   )
 }
