6f71ba00d946374f4f9d009b665b32db576b9863
Fairytale-DEF <farid.djerrar@efrei.net>
2025-07-31 15:21:32 +0200

    feat: enhance SettingsScreen with gradient background and header styling

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,11 +1,40 @@
-import React from "react";
+import { createSettingsStyles } from "@/assets/styles/settings.styles";
+import useTheme from "@/hooks/useTheme";
+import { Ionicons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import React, { useState } from "react";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SettingsScreen = () => {
+
+  const [isAutoSync, setIsAutoSync] = useState(true);
+  const [isNotificationsEnabled, setIsNotificationsEnabled] = useState(true);
+
+  const {colors, isDarkMode, toggleDarkMode} = useTheme();
+  const settingsStyle = createSettingsStyles(colors);
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={settingsStyle.container}
+    >
+      <SafeAreaView style={settingsStyle.safeArea}>
+        {/* HEADER */}
+        <View style={settingsStyle.header}>
+          <View style={settingsStyle.titleContainer}>
+            <LinearGradient
+              colors={colors.gradients.primary}
+              style={settingsStyle.iconContainer}
+            >
+              <Ionicons name="settings" size={32} color={colors.text} />
+            </LinearGradient>
+            <Text style={settingsStyle.title}>Réglages</Text>
+
+          </View>
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
